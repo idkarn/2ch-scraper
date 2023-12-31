0a1981a6b51d3268f3c779bb11e50d1dacfe18ca
Kir Ivanov <kirivanov777@gmail.com>
2023-12-31 03:03:42 +0300

    added filters for proxy

--- proxy/server.py
+++ proxy/server.py
@@ -2,25 +2,37 @@ import asyncio
 from json import load, dump
 import random
 
-from sanic import Sanic, text
+from sanic import Sanic, json, Request
 from proxy import get_proxies, is_working
 
 app = Sanic("proxy-manager", configure_logging=False)
 proxy_list: dict[str, list[str]] = {}
 
 
-@app.route("/proxy")
-async def proxy(req):
-    proto = "HTTP" if random.random() < 0.5 else "SOCKS5"
+async def get_working_proxy(p: str) -> str:
+    def get_proto():
+        return p.upper() or ("HTTP" if random.random() < 0.5 else "SOCKS5")
+
+    proto = get_proto()
     addr = random.choice(proxy_list[proto])
 
     while not await is_working(proto, addr):
         proxy_list[proto].remove(addr)
 
-        proto = "HTTP" if random.random() < 0.5 else "SOCKS5"
+        proto = get_proto()
         addr = random.choice(proxy_list[proto])
 
-    return text(f"{proto.lower()}://{addr}")
+    return f"{proto.lower()}://{addr}"
+
+
+@app.route("/proxy")
+async def handle_proxy(req: Request):
+    n = int(req.args["n"][0]) if "n" in req.args else 1
+    proxy_list = [
+        await get_working_proxy(req.args["p"][0] if "p" in req.args else "")
+        for _ in range(n)
+    ]
+    return json(proxy_list)
 
 
 def load_proxy_list() -> dict[str, list[str]] | None:

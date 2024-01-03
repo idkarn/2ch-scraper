993fff8db3cf19db67f12e79c683eec73dd160bc
Kir Ivanov <kirivanov777@gmail.com>
2024-01-03 03:12:10 +0300

    implemented linksDB

--- app/pkg/storage/db.go
+++ app/pkg/storage/db.go
@@ -1,34 +1,47 @@
 package storage
 
 import (
+	"encoding/binary"
 	"log"
 	"net/url"
 
 	"github.com/akrylysov/pogreb"
 )
 
+const DB_PATH = "./app/links.db"
+
 type LinkDB struct {
 	conn *pogreb.DB
 }
 
 func newDB() *LinkDB {
-	db, err := pogreb.Open("./app/links.db", nil)
+	db, err := pogreb.Open(DATA_PATH, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 	return &LinkDB{db}
 }
 
+func uint64ToBytes(val uint64) []byte {
+	key := make([]byte, 8)
+	binary.NativeEndian.PutUint64(key, val)
+	return key
+}
+
 func (db *LinkDB) Init() error {
-	return nil
+	var err error
+	if db.conn == nil {
+		db.conn, err = pogreb.Open(DB_PATH, nil)
+	}
+	return err
 }
 
 func (db *LinkDB) Visited(requestID uint64) error {
-	return nil
+	return db.conn.Put(uint64ToBytes(requestID), nil)
 }
 
 func (db *LinkDB) IsVisited(requestID uint64) (bool, error) {
-	return false, nil
+	return db.conn.Has(uint64ToBytes(requestID))
 }
 
 func (db *LinkDB) Cookies(u *url.URL) string {
